46648ada4d89a59289925236c8398d365ff654e7
desiirenav <desiirenav@hotmail.com>
2025-09-12 17:49:40 -0400

    Update plugins.lua

--- nvim/plugin/plugins.lua
+++ nvim/plugin/plugins.lua
@@ -2,8 +2,3 @@ if vim.g.did_load_plugins_plugin then
   return
 end
 vim.g.did_load_plugins_plugin = true
-
--- many plugins annoyingly require a call to a 'setup' function to be loaded,
--- even with default configs
-
-require('nvim-surround').setup()
