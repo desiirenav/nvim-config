082d23df1efb635190305efa4d758c5e2e7ea482
desiirenav <desiirenav@hotmail.com>
2025-10-05 21:09:17 -0400

    Update neovim-overlay.nix

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -32,7 +32,7 @@ with final.pkgs.lib; let
     # https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=vimPlugins
     # colorscheme
     oxocarbon-nvim
-    lillilac
+    awesome-neovim
     nvim-treesitter.withAllGrammars
     luasnip # snippets | https://github.com/l3mon4d3/luasnip/
     # nvim-cmp (autocompletion) and extensions
